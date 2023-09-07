b11d6bacba597411545696ebf50d3f8decf08716
Artem <artemvandalovski@gmail.com>
2023-09-07 11:22:19 -0400

    Added Spring Project

---
+++ audace/src/main/java/com/equipe4/audace/AudaceApplication.java
@@ -0,0 +1,13 @@
+package com.equipe4.audace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudaceApplication.class, args);
+	}
+
+}
