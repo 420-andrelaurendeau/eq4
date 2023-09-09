1a2e455e3efdc2794c57ddbc941986e82da401f0
Arson <binauralblues@gmail.com>
2023-09-09 17:34:29 -0400

    EQa-58-Methode createEmployer dans EmployerController

--- audace-backend/src/main/java/com/equipe4/audace/controller/EmployerController.java
+++ audace-backend/src/main/java/com/equipe4/audace/controller/EmployerController.java
@@ -1,7 +1,26 @@
 package com.equipe4.audace.controller;
 
+import com.equipe4.audace.dto.EmployerDTO;
+import com.equipe4.audace.service.EmployerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//TODO: API
 @RestController
 public class EmployerController {
+    private final EmployerService employerService;
+
+    public EmployerController(EmployerService employerService) {
+        this.employerService = employerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployerDTO> createEmployer(@RequestBody EmployerDTO employerDTO){
+        return employerService.saveEmployer(employerDTO)
+                .map(employer -> ResponseEntity.status(HttpStatus.OK).body(employerDTO))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
