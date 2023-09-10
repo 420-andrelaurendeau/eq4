1c0fa25a0ce9ab7bcf795386ce9effdede3d246e
joshua kuzak <joshua_938@outlook.com>
2023-09-10 17:24:29 -0400

    EQ-4-41-Added react-i18n to project and created base for i18n in FE

--- audace-frontend/src/App.tsx
+++ audace-frontend/src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const {t} = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{t("temp")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
