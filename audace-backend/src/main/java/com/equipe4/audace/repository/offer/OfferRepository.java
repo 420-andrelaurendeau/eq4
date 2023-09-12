3520db4e5604733cc7033df6baeea88724a500c9
joshua kuzak <joshua_938@outlook.com>
2023-09-12 16:08:05 -0400

    EQ-4-154-Implemented getOffersByDepartment

--- audace-backend/src/main/java/com/equipe4/audace/repository/offer/OfferRepository.java
+++ audace-backend/src/main/java/com/equipe4/audace/repository/offer/OfferRepository.java
@@ -4,8 +4,9 @@ import com.equipe4.audace.model.department.Department;
 import com.equipe4.audace.model.offer.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
-    Optional<Offer> findByDepartment(Department department);
+    List<Offer> findAllByDepartment(Department department);
 }
--- audace-backend/src/main/java/com/equipe4/audace/service/StudentService.java
+++ audace-backend/src/main/java/com/equipe4/audace/service/StudentService.java
@@ -1,13 +1,40 @@
 package com.equipe4.audace.service;
 
 import com.equipe4.audace.dto.StudentDTO;
+import com.equipe4.audace.dto.offer.OfferDTO;
+import com.equipe4.audace.model.department.Department;
+import com.equipe4.audace.model.offer.Offer;
 import com.equipe4.audace.repository.StudentRepository;
+import com.equipe4.audace.repository.department.DepartmentRepository;
+import com.equipe4.audace.repository.offer.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class StudentService {
-    StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
+    private final OfferRepository offerRepository;
+    private final DepartmentRepository departmentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository, OfferRepository offerRepository, DepartmentRepository departmentRepository) {
+        this.studentRepository = studentRepository;
+        this.offerRepository = offerRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
     public void createStudent(StudentDTO studentDTO) {
         studentRepository.save(studentDTO.fromDTO());
+    }
+
+    @Transactional
+    public List<OfferDTO> getOffersByDepartment(Long departmentId) {
+        Department department = departmentRepository.findById(departmentId).orElseThrow();
+        List<Offer> offers = offerRepository.findAllByDepartment(department);
+
+        return offers.stream().map(Offer::toDto).toList();
     }
 }
