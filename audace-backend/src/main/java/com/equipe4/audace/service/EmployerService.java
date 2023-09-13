b17c39a50900a35932825ccfc16cec9336b48be1
Arson <binauralblues@gmail.com>
2023-09-13 14:43:12 -0400

    EQ4-58-Modification dans EmployerService

--- audace-backend/src/main/java/com/equipe4/audace/service/EmployerService.java
+++ audace-backend/src/main/java/com/equipe4/audace/service/EmployerService.java
@@ -1,24 +1,27 @@
 package com.equipe4.audace.service;
 
 import com.equipe4.audace.dto.EmployerDTO;
-import com.equipe4.audace.model.Employer;
 import com.equipe4.audace.repository.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployerService {
     private final EmployerRepository employerRepository;
+
+    @Autowired
+    public EmployerService(EmployerRepository employerRepository) {
+        this.employerRepository = employerRepository;
+    }
 
     public Optional<EmployerDTO> saveEmployer(EmployerDTO employerDTO){
         return Optional.of(new EmployerDTO(employerRepository.save(employerDTO.fromDTO())));
     }
 
     public List<EmployerDTO> findAllEmployers(){
-        List<EmployerDTO> dtos = new ArrayList<>();
-        for(Employer employer: employerRepository.findAll()) dtos.add(new EmployerDTO(employer));
-
-        return dtos;
+        return employerRepository.findAll().stream().map(EmployerDTO::new).toList();
     }
 }
