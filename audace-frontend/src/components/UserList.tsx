aef8d98facece7ffe0d6840b86f367f6d805c70a
p3terson7 <psarateanu@hotmail.ca>
2023-09-14 16:38:11 -0400

    frontend: style

--- audace-frontend/src/components/UserList.tsx
+++ audace-frontend/src/components/UserList.tsx
@@ -5,17 +5,20 @@ const UserList: React.FC = () => {
     const [users, setUsers] = useState<any[]>([]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/users')
+        fetch('http://localhost:3000/users')
             .then((response) => response.json())
-            .then((data) => setUsers(data));
+            .then((data) => {
+                console.log(data);
+                setUsers(data);
+            });
     }, []);
 
     return (
         <div className={"container vh-100"}>
             <h1 className={"m-5"}>Login (temporaire)</h1>
-            <div className={"container-fluid mw-100"} style={{background: '#ccc', width: '35%' }}>
+            <div className={"container-fluid mw-100"} style={{background: '#ccc', width: '35%', borderRadius: '5px'}}>
                 <div className="d-flex justify-content-between p-2">
-                    <div className="d-inline-block">lil</div>
+                    <div className="d-inline-block align-self-center" style={{}}>Username</div>
                     <div className="d-inline-block btn btn-success">✓</div>
                 </div>
 
