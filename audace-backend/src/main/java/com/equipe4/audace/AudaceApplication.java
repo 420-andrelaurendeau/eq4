2db5810e3960308780f600256cd995261b4f2a8d
p3terson7 <psarateanu@hotmail.ca>
2023-09-20 15:35:23 -0400

    clean frontend

--- audace-backend/src/main/java/com/equipe4/audace/AudaceApplication.java
+++ audace-backend/src/main/java/com/equipe4/audace/AudaceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AudaceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AudaceApplication.class, args);
 	}
-
 }
--- audace-frontend/src/components/UserList.tsx
+++ audace-frontend/src/components/UserList.tsx
@@ -1,10 +1,10 @@
 
 import React, { useEffect, useState } from 'react';
 import service from "../DataService/service";
-import { User } from "../model/user"
+import {User} from "../model/user";
 
 const UserList: React.FC = () => {
-    const [users, setUsers] = useState<any[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
 
     useEffect(() => {
         service.getAllUsers()
@@ -14,21 +14,18 @@ const UserList: React.FC = () => {
     }, []);
 
     return (
-        <div className={"container vh-100"}>
-            <h1 className={"m-5"}>Login (temporaire)</h1>
-            <div className={"container-fluid mw-100"} style={{background: '#ccc', width: '35%', borderRadius: '5px'}}>
-                <div className="d-flex justify-content-between p-2">
-                    <div className="d-inline-block align-self-center" style={{}}>ID</div>
-                    <div className="d-inline-block btn btn-success">+</div>
-                </div>
-
-            </div>
             <ul>
                 {users.map((user) => (
-                    <li key={user.id}>{user.email}</li>
+                    <li key={user.id}>
+                        <div className={"container-fluid mw-100"} style={{background: '#ccc', width: '35%', borderRadius: '5px'}}>
+                            <div className="d-flex justify-content-between p-2">
+                                <div className="d-inline-block align-self-center">{user.email}</div>
+                                <div className="d-inline-block btn btn-success fw-bold">Sign in</div>
+                            </div>
+                        </div>
+                    </li>
                 ))}
             </ul>
-        </div>
     );
 };
 
