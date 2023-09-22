854cfb1cdcaab83f814558bc72feac4eb24f366b
joshua kuzak <joshua_938@outlook.com>
2023-09-22 16:02:15 -0400

    EQ-4-93-Fleshed out OfferModal and fixed styling errors caused by global styling BS

--- audace-frontend/src/components/StudentOffersList/StudentOffer/OfferModal/index.tsx
+++ audace-frontend/src/components/StudentOffersList/StudentOffer/OfferModal/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Offer } from "../../../../model/offer";
 import { Employer } from "../../../../model/user";
-import { Modal } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import { formatDate } from "..";
 
 interface Props {
     offer: Offer;
@@ -13,15 +14,52 @@ const OfferModal = ({offer, show, handleClose}: Props) => {
     const [employer, setEmployer] = useState<Employer>();
 
     useEffect(() => {
+        let employer: Employer = {
+            id: offer.employerId,
+            firstName: "John",
+            lastName: "Doe",
+            address: "123 Fake Street",
+            phone: "1234567890",
+            email: "",
+            password: "",
+            organisation: "Fake Organisation",
+            extension: "fake",
+            position: "Fake Position",
+            offers: []
+        }
 
-    });
+        setEmployer(employer);
+    }, [setEmployer, offer]);
+
+    const applyToOffer = () => {
+        // Not in this story's scope
+    };
+
+    const createBoldText = (text: string) => {
+        return <b>{text}</b>;
+    };
 
     return (
         <>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleClose} size="lg">
                 <Modal.Header closeButton>
                     <Modal.Title>{offer.title}</Modal.Title>
                 </Modal.Header>
+                <Modal.Body>
+                    <div className="text-end">
+                        <div>Organization: {createBoldText(employer?.organisation!)}</div>
+                        <div>From : {createBoldText(formatDate(offer.internshipStartDate))} to <b>{createBoldText(formatDate(offer.internshipEndDate))}</b></div>
+                        <div>Offer ends: {createBoldText(formatDate(offer.offerEndDate))}</div>
+                    </div>
+
+                    <hr/>
+
+                    <u><h4 className="my-3 text-center">Offer description</h4></u>
+                    <div style={{textAlign : "justify"}}>{offer.description}</div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button className="btn-success" onClick={applyToOffer}>Apply</Button>
+                </Modal.Footer>
             </Modal>
         </>
     );
--- audace-frontend/src/components/StudentOffersList/StudentOffer/index.tsx
+++ audace-frontend/src/components/StudentOffersList/StudentOffer/index.tsx
@@ -7,29 +7,24 @@ interface Props {
     offer: Offer;
 }
 
+export const formatDate = (date: Date) => {
+    const newDate = new Date(date);
+    const year = newDate.getFullYear();
+    const month = newDate.getMonth() + 1;
+    const day = newDate.getDate();
+
+    return `${day}/${month}/${year}`;
+}
+
 const StudentOffer = ({offer}: Props) => {
     const [show, setShow] = useState<boolean>(false);
 
-    const formatDate = (date: Date) => {
-        const newDate = new Date(date);
-        const year = newDate.getFullYear();
-        const month = newDate.getMonth() + 1;
-        const day = newDate.getDate();
-
-        return `${day}/${month}/${year}`;
-    }
-
-    const handleClick = () => {
-        setShow(true);
-    };
-
-    const handleClose = () => {
-        setShow(false);
-    };
+    const handleClick = () => setShow(true);
+    const handleClose = () => setShow(false);
 
     return (
         <>
-            <tr onClick={handleClick}>
+            <tr className="hovered" onClick={handleClick}>
                 <td>{offer.title}</td>
                 <td>{formatDate(offer.internshipStartDate)}</td>
                 <td>{formatDate(offer.internshipEndDate)}</td>
