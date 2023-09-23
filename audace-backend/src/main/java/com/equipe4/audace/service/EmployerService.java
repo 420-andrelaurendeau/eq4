e4ecf31110e7675316e0a1dd02b1368d586fb5c9
Arson <binauralblues@gmail.com>
2023-09-23 13:31:20 -0400

    EQ4-5 Modification de la classe EmployerService

--- audace-backend/src/main/java/com/equipe4/audace/service/EmployerService.java
+++ audace-backend/src/main/java/com/equipe4/audace/service/EmployerService.java
@@ -1,6 +1,7 @@
 package com.equipe4.audace.service;
 
 import com.equipe4.audace.dto.EmployerDTO;
+import com.equipe4.audace.model.Employer;
 import com.equipe4.audace.repository.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,12 @@ public class EmployerService {
     }
 
     public Optional<EmployerDTO> createEmployer(EmployerDTO employerDTO){
+        if (employerDTO == null) throw new IllegalArgumentException("Employer cannot be null");
+
+        Optional<Employer> employerOptional = employerRepository.findByEmail(employerDTO.getEmail());
+
+        if (employerOptional.isPresent()) throw new IllegalArgumentException("Email already in use");
+
         return Optional.of(new EmployerDTO(employerRepository.save(employerDTO.fromDTO())));
     }
 
