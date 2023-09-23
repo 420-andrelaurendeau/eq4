edb5c3b323113cc8e9bc86a706cb75d915bb7a15
joshua kuzak <joshua_938@outlook.com>
2023-09-23 11:39:02 -0400

    EQ-4-94-Increased max length of description string in DB

--- audace-backend/src/main/java/com/equipe4/audace/model/offer/Offer.java
+++ audace-backend/src/main/java/com/equipe4/audace/model/offer/Offer.java
@@ -3,10 +3,7 @@ package com.equipe4.audace.model.offer;
 import com.equipe4.audace.dto.offer.OfferDTO;
 import com.equipe4.audace.model.Employer;
 import com.equipe4.audace.model.department.Department;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +20,8 @@ public class Offer {
     private Long id;
 
     private String title;
+
+    @Column(length = 2048)
     private String description;
     private Date internshipStartDate;
     private Date internshipEndDate;
