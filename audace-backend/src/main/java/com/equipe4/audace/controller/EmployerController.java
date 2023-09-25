3aa75047c290acf11bb02835fdde5e0050f79c91
Arson <binauralblues@gmail.com>
2023-09-25 14:05:13 -0400

    EQ4-64-Creation du classe OfferController

--- audace-backend/src/main/java/com/equipe4/audace/controller/EmployerController.java
+++ audace-backend/src/main/java/com/equipe4/audace/controller/EmployerController.java
@@ -2,9 +2,9 @@ package com.equipe4.audace.controller;
 
 import com.equipe4.audace.dto.EmployerDTO;
 import com.equipe4.audace.service.EmployerService;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +19,7 @@ public class EmployerController {
     private final EmployerService employerService;
     private Logger logger = LoggerFactory.getLogger(EmployerController.class);
 
+    @Autowired
     public EmployerController(EmployerService employerService) {
         this.employerService = employerService;
     }
--- audace-backend/src/main/java/com/equipe4/audace/controller/OfferController.java
+++ audace-backend/src/main/java/com/equipe4/audace/controller/OfferController.java
@@ -1,2 +1,50 @@
-package com.equipe4.audace.controller;public class OfferController {
+package com.equipe4.audace.controller;
+
+import com.equipe4.audace.dto.offer.OfferDTO;
+import com.equipe4.audace.service.OfferService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/offers")
+public class OfferController {
+    private final OfferService offerService;
+    private Logger logger = LoggerFactory.getLogger(OfferController.class);
+
+
+    @Autowired
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping
+    public List<OfferDTO> getAllOffers(){
+        logger.info("getAllOffers");
+        return offerService.findAllOffers();
+    }
+    @PostMapping
+    public ResponseEntity<HttpStatus> createOffer(@RequestBody OfferDTO offerDTO){
+        logger.info("createOffer");
+        offerService.createOffer(offerDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @PutMapping
+    public ResponseEntity updateOffer(@RequestBody OfferDTO offerDTO){
+        logger.info("updateOffer");
+        OfferDTO updatedOffer = offerService.updateOffer(offerDTO).get();
+        return ResponseEntity.ok(updatedOffer);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteOffer(@RequestParam("offerId") Long offerId){
+        offerService.deleteOffer(offerId);
+        return ResponseEntity.ok().build();
+    }
 }
