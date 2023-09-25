b6037d009ee34ec75f0be99c52dc98a39365173d
Arson <binauralblues@gmail.com>
2023-09-25 14:25:50 -0400

    EQ4-64-Correction dans AudaceApplication

--- audace-backend/src/main/java/com/equipe4/audace/AudaceApplication.java
+++ audace-backend/src/main/java/com/equipe4/audace/AudaceApplication.java
@@ -6,11 +6,13 @@ import com.equipe4.audace.model.offer.Offer;
 import com.equipe4.audace.repository.EmployerRepository;
 import com.equipe4.audace.repository.department.DepartmentRepository;
 import com.equipe4.audace.repository.offer.OfferRepository;
+import com.equipe4.audace.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @SpringBootApplication
@@ -19,6 +21,14 @@ public class AudaceApplication implements CommandLineRunner {
 	private DepartmentRepository departmentRepository;
 	@Autowired
 	private EmployerRepository employerRepository;
+	private OfferService offerService;
+
+	public AudaceApplication(DepartmentRepository departmentRepository, EmployerRepository employerRepository, OfferService offerService) {
+		this.departmentRepository = departmentRepository;
+		this.employerRepository = employerRepository;
+		this.offerService = offerService;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AudaceApplication.class, args);
 	}
@@ -27,7 +37,6 @@ public class AudaceApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Department department = departmentRepository.save(new Department("GLO", "Génie logiciel"));
 		Employer employer = new Employer(
-				null,
 				"employer",
 				"employerman",
 				"temp@gmail.com",
@@ -39,10 +48,29 @@ public class AudaceApplication implements CommandLineRunner {
 				"extension"
 		);
 
-		Offer offer1 = new Offer("Stage en génie logiciel", "Stage en génie logiciel", new Date(), new Date(), null, employer, department);
-		Offer offer4 = new Offer("Stage en génie logiciel", "Stage en génie logiciel", new Date(), new Date(), null, employer, department);
-		Offer offer2 = new Offer("Stage en génie logiciel", "Stage en génie logiciel", new Date(), new Date(), null, employer, department);
-		Offer offer3 = new Offer("Stage en génie logiciel", "Stage en génie logiciel", new Date(), new Date(), null, employer, department);
+		Offer offer1 = Offer.offerBuilder()
+				.title("Stage en génie logiciel").description("Stage en génie logiciel")
+				.internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+				.employer(employer).department(department)
+				.build();
+		Offer offer2 = Offer.offerBuilder()
+				.title("Stage en génie logiciel").description("Stage en génie logiciel")
+				.internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+				.department(department)
+				.build();
+		Offer offer3 = Offer.offerBuilder()
+				.title("Stage en génie logiciel").description("Stage en génie logiciel")
+				.internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+				.department(department)
+				.build();
+		Offer offer4 = Offer.offerBuilder()
+				.title("Stage en génie logiciel").description("Stage en génie logiciel")
+				.internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+				.department(department)
+				.build();
+
+
+
 		employer.getOffers().add(offer1);
 		employer.getOffers().add(offer2);
 		employer.getOffers().add(offer3);
