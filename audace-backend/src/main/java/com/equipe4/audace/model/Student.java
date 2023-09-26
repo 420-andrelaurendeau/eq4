7d3962ef49c4677cec3f7d67965cd1d101ddb522
joshua kuzak <joshua_938@outlook.com>
2023-09-26 13:02:21 -0400

    EQ-4-79-Added list of cvs to Student

--- audace-backend/src/main/java/com/equipe4/audace/model/Student.java
+++ audace-backend/src/main/java/com/equipe4/audace/model/Student.java
@@ -1,6 +1,7 @@
 package com.equipe4.audace.model;
 
 import com.equipe4.audace.dto.StudentDTO;
+import com.equipe4.audace.model.cv.Cv;
 import com.equipe4.audace.model.department.Department;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +10,9 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance
@@ -20,6 +24,9 @@ public class Student extends User {
 
     @ManyToOne(cascade = CascadeType.ALL)
     private Department department;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Cv> cvs;
 
     public Student(Long id,
                    String firstname,
@@ -33,6 +40,23 @@ public class Student extends User {
         super(id, firstname, lastname, email, password, address, phone);
         this.studentNumber = studentNumber;
         this.department = department;
+        this.cvs = new ArrayList<>();
+    }
+
+    public Student(Long id,
+                   String firstname,
+                   String lastname,
+                   String email,
+                   String password,
+                   String address,
+                   String phone,
+                   String studentNumber,
+                   Department department,
+                   List<Cv> cvs) {
+        super(id, firstname, lastname, email, password, address, phone);
+        this.studentNumber = studentNumber;
+        this.department = department;
+        this.cvs = cvs;
     }
 
     public StudentDTO toDTO() {
