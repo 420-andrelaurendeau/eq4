8acf372fb0dee68585458145fb70e9e0d71edfe8
Arson <binauralblues@gmail.com>
2023-09-28 16:25:00 -0400

    EQ4-64-OfferController Update test

--- audace-backend/src/test/java/com/equipe4/audace/controller/OfferControllerTest.java
+++ audace-backend/src/test/java/com/equipe4/audace/controller/OfferControllerTest.java
@@ -22,15 +22,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(OfferController.class)
@@ -88,6 +89,94 @@ public class OfferControllerTest {
                 .andExpect(jsonPath("$.approved", is(offerDTO.isApproved())))
                 .andExpect(jsonPath("$.employerId", is(offerDTO.getEmployerId().intValue())))
                 .andExpect(jsonPath("$.departmentCode", is(offerDTO.getDepartmentCode())));
+    }
+
+    @Test
+    public void givenListOfOffers_whenGetAllOffers_thenReturnOffersList() throws Exception{
+        // given - precondition or setup
+        Department department = new Department("GLO", "Génie logiciel");
+        Employer employer = Employer.employerBuilder()
+                .firstName("Employer1").lastName("Employer1").email("employer1@gmail.com").password("123456eE")
+                .organisation("Organisation1").position("Position1").phone("123-456-7890").extension("12345")
+                .address("Class Service, Javatown, Qc H8N1C1")
+                .build();
+        employer.setId(1L);
+
+        List<OfferDTO> listOfOffers = new ArrayList<>();
+        listOfOffers.add(OfferDTO.offerDTOBuilder()
+                .title("Stage en génie logiciel").description("Stage en génie logiciel")
+                .internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+                .availablePlaces(3).employerId(employer.getId()).departmentCode(department.getCode())
+                .build());
+        listOfOffers.add(OfferDTO.offerDTOBuilder()
+                .title("Stage en génie logiciel").description("Stage en génie logiciel")
+                .internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+                .availablePlaces(5).employerId(employer.getId()).departmentCode(department.getCode())
+                .build());
+        given(offerService.findAllOffers()).willReturn(listOfOffers);
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/offers"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()",
+                        is(listOfOffers.size())));
+
+    }
+
+    @Test
+    public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdateEmployeeObject() throws Exception{
+        // given - precondition or setup
+
+        Department department = new Department("GLO", "Génie logiciel");
+        Employer employer = Employer.employerBuilder()
+                .firstName("Employer1").lastName("Employer1").email("employer1@gmail.com").password("123456eE")
+                .organisation("Organisation1").position("Position1").phone("123-456-7890").extension("12345")
+                .address("Class Service, Javatown, Qc H8N1C1")
+                .build();
+        employer.setId(1L);
+
+        Offer offerSaved = Offer.offerBuilder()
+                .title("Stage en génie logiciel").description("Stage en génie logiciel")
+                .internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+                .availablePlaces(3).employer(employer).department(department)
+                .build();
+        offerSaved.setId(1L);
+        OfferDTO offerDTOSaved = new OfferDTO(offerSaved);
+
+        Offer offerUpdated = Offer.offerBuilder()
+                .title("Stage en génie logiciel").description("Stage en génie logiciel")
+                .internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+                .availablePlaces(5).employer(employer).department(department)
+                .build();
+        offerUpdated.setId(1L);
+        OfferDTO offerDTOUpdated = new OfferDTO(offerUpdated);
+
+
+        given(offerService.findOfferById(offerDTOSaved.getId())).willReturn(offerSaved);
+        given(offerService.updateOffer(any(OfferDTO.class))).willReturn(Optional.of(offerDTOUpdated));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/offers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(offerDTOUpdated)));
+
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id", is(offerDTOUpdated.getId().intValue())))
+                .andExpect(jsonPath("$.title", is(offerDTOUpdated.getTitle())))
+                .andExpect(jsonPath("$.description", is(offerDTOUpdated.getDescription())))
+                .andExpect(jsonPath("$.internshipStartDate", is(offerDTOUpdated.getInternshipStartDate().toString())))
+                .andExpect(jsonPath("$.internshipEndDate", is(offerDTOUpdated.getInternshipEndDate().toString())))
+                .andExpect(jsonPath("$.offerEndDate", is(offerDTOUpdated.getOfferEndDate().toString())))
+                .andExpect(jsonPath("$.availablePlaces", is(offerDTOUpdated.getAvailablePlaces())))
+                .andExpect(jsonPath("$.approved", is(offerDTOUpdated.isApproved())))
+                .andExpect(jsonPath("$.employerId", is(offerDTOUpdated.getEmployerId().intValue())))
+                .andExpect(jsonPath("$.departmentCode", is(offerDTOUpdated.getDepartmentCode())));
     }
 
 
