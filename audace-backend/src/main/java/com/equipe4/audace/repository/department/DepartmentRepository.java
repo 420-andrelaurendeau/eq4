333cee796d11b74d9eb56333904f338b6718caf0
Arson <binauralblues@gmail.com>
2023-10-02 15:09:45 -0400

    Merge from EQ4-28

--- audace-backend/src/main/java/com/equipe4/audace/repository/department/DepartmentRepository.java
+++ audace-backend/src/main/java/com/equipe4/audace/repository/department/DepartmentRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<Department> findByCode(String code);
-    boolean existsByCode(String code);
 }
