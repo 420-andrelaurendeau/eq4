bbde8b5c3f592fd95b1e6c654ae9aae0e6134264
joshua kuzak <joshua_938@outlook.com>
2023-10-04 11:49:19 -0400

    EQ-4-142-Removed unused service function

--- audace-backend/src/main/java/com/equipe4/audace/service/UserService.java
+++ audace-backend/src/main/java/com/equipe4/audace/service/UserService.java
@@ -17,8 +17,6 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void createUser(UserDTO userDTO) {}
-
     public List<UserDTO> getAllUsers() {
         return userRepository.findAll().stream().map(User::toDTO).toList();
     }
