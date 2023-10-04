0f0a1d0f5155506a6f7457a028c78a728b100710
Arson <binauralblues@gmail.com>
2023-10-04 18:35:26 -0400

    EQ4-106-Controller test pour creer candidature

--- audace-backend/src/test/java/com/equipe4/audace/controller/StudentControllerTest.java
+++ audace-backend/src/test/java/com/equipe4/audace/controller/StudentControllerTest.java
@@ -1,9 +1,14 @@
 package com.equipe4.audace.controller;
 
+import com.equipe4.audace.dto.ApplicationDTO;
 import com.equipe4.audace.dto.StudentDTO;
 import com.equipe4.audace.dto.offer.OfferDTO;
+import com.equipe4.audace.model.Application;
+import com.equipe4.audace.model.Employer;
 import com.equipe4.audace.model.Student;
+import com.equipe4.audace.model.cv.Cv;
 import com.equipe4.audace.model.department.Department;
+import com.equipe4.audace.model.offer.Offer;
 import com.equipe4.audace.repository.EmployerRepository;
 import com.equipe4.audace.repository.ManagerRepository;
 import com.equipe4.audace.repository.StudentRepository;
@@ -11,6 +16,7 @@ import com.equipe4.audace.repository.department.DepartmentRepository;
 import com.equipe4.audace.repository.offer.OfferRepository;
 import com.equipe4.audace.service.EmployerService;
 import com.equipe4.audace.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import com.equipe4.audace.dto.cv.CvDTO;
 import com.equipe4.audace.repository.EmployerRepository;
@@ -28,16 +34,20 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
@@ -51,6 +61,8 @@ import static org.mockito.Mockito.*;
 public class StudentControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private StudentService studentService;
@@ -143,5 +155,58 @@ public class StudentControllerTest {
 
         mockMvc.perform(get("/students/1"))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenApplicationObject_whenCreateApplication_thenReturnSavedApplication() throws Exception{
+        // given - precondition or setup
+        Department department = new Department("GLO", "Génie logiciel");
+        Employer employer = Employer.employerBuilder()
+                .firstName("Employer1").lastName("Employer1").email("employer1@gmail.com").password("123456eE")
+                .organisation("Organisation1").position("Position1").phone("123-456-7890").extension("12345")
+                .address("Class Service, Javatown, Qc H8N1C1")
+                .build();
+        employer.setId(1L);
+        Student student = new Student(
+                1L,
+                "student",
+                "studentman",
+                "email@email.com",
+                "password",
+                "address",
+                "phone",
+                "matricule",
+                department
+        );
+        Cv cv = new Cv();
+        cv.setId(1L);
+
+        Offer offer = Offer.offerBuilder()
+                .title("Stage en génie logiciel").description("Stage en génie logiciel")
+                .internshipStartDate(LocalDate.now()).internshipEndDate(LocalDate.now()).offerEndDate(LocalDate.now())
+                .availablePlaces(3).employer(employer).department(department)
+                .build();
+        offer.setId(1L);
+        Application application = Application.applicationBuilder()
+                .student(student)
+                .cv(cv)
+                .offer(offer)
+                .build();
+        ApplicationDTO applicationDTO = new ApplicationDTO(application);
+
+        when(studentService.createApplication(any(ApplicationDTO.class))).thenReturn(Optional.of(applicationDTO));
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/students/{id}/applications", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(Optional.of(applicationDTO))));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(applicationDTO.getId())))
+                .andExpect(jsonPath("$.studentId", is(applicationDTO.getStudentId().intValue())))
+                .andExpect(jsonPath("$.cvId", is(applicationDTO.getCvId().intValue())))
+                .andExpect(jsonPath("$.offerId", is(applicationDTO.getOfferId().intValue())));
     }
 }
