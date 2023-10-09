c7bcbebd40592f8271fb2f2d6f4b201f356d26bb
joshua kuzak <joshua_938@outlook.com>
2023-10-09 00:20:27 -0400

    EQ-4-54-i18n for logout button

--- audace-frontend/src/components/LogoutButton/index.tsx
+++ audace-frontend/src/components/LogoutButton/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "react-bootstrap";
 import { logout } from "../../services/authService";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+  const {t} = useTranslation();
 
   const onClick = () => {
     logout();
@@ -13,7 +15,7 @@ const LogoutButton = () => {
   return (
     <>
       <Button variant="light" onClick={onClick}>
-        Logout
+        {t("logout")}
       </Button>
     </>
   );
