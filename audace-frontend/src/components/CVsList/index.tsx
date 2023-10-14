8ffe13a04a51fed6e31026f3c4c56c1154bae130
p3terson7 <psarateanu@hotmail.ca>
2023-10-14 17:56:38 -0400

    Bootstrap sur liste CV's

--- audace-frontend/src/components/CVsList/index.tsx
+++ audace-frontend/src/components/CVsList/index.tsx
@@ -21,19 +21,23 @@ function CvList() {
     }, [studentId]);
 
     return (
-        <div>
+        <div className="container my-3 g-0">
             <h2>Your CVs</h2>
-            {cvs.length > 0 ? (
-                <ul>
-                    {cvs.map((cv) => (
-                        <li key={cv.id}>
-                            <a href={`link-to-cv/${cv.id}`}>{cv.fileName}</a>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No CVs found</p>
-            )}
+            <div className="card">
+                <div className="card-body">
+                    {cvs.length > 0 ? (
+                        <ul className="list-group">
+                            {cvs.map((cv) => (
+                                <li key={cv.id} className="list-group-item">
+                                    <a href={`link-to-cv/${cv.id}`}>{cv.fileName}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No CVs found</p>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
