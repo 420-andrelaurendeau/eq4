f92f1f850c2f640ffed53ce0b94daafcf35550c8
CedricBEmond <111804930+CedricBEmond@users.noreply.github.com>
2023-10-18 21:10:52 -0400

    This might be shit IDK that's what we live for (#27)
    * I heard yeah
    
    * might work IDK I don,t fucking care no more
    
    * Delete audace-frontend/src directory
    
    * source 2 kinda like CS2 but fuckier
    
    * made backend shit run

--- audace-frontend/src/components/CVsList/index.tsx
+++ audace-frontend/src/components/CVsList/index.tsx
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import {CV} from "../../model/cv";
-import {getCvsByStudentId} from "../../services/studentApplicationService";
-import {getUserId} from "../../services/authService";
+import { Table } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { UserType } from "../../model/user";
+import { CV, CVStatus } from "../../model/cv";
+import CvRow from "./CvRow";
 
-function CvList() {
-    const [cvs, setCvs] = useState<CV[]>([]); // Initialize state for CVs
-    const studentId = getUserId();
-
-    useEffect(() => {
-        if (studentId) {
-            // Fetch CVs for the current student
-            getCvsByStudentId(Number(studentId))
-                .then((response) => {
-                    setCvs(response.data);
-                })
-                .catch((error) => {
-                    console.error('Error fetching CVs:', error);
-                });
-        }
-    }, [studentId]);
-
-    return (
-        <div className="container my-3 g-0">
-            <h2>Your CVs</h2>
-            <div className="card">
-                <div className="card-body">
-                    {cvs.length > 0 ? (
-                        <ul className="list-group">
-                            {cvs.map((cv) => (
-                                <li key={cv.id} className="list-group-item">
-                                    <a href={`link-to-cv/${cv.id}`}>{cv.fileName}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>No CVs found</p>
-                    )}
-                </div>
-            </div>
-        </div>
-    );
+interface Props {
+    cvs: CV[];
+    error: string;
+    userType: UserType;
+    updateCvsState?: (cv : CV, cvStatus : CVStatus) => void;
 }
 
-export default CvList;
+const CvsList = ({cvs, error, userType, updateCvsState}: Props) => {
+    const {t} = useTranslation();
+
+    return (
+        <>
+            {
+                error !== "" 
+                ? 
+                <p>{error}</p> 
+                : 
+                cvs.length > 0
+                    ?
+                    <Table striped bordered size="sm">
+                        <thead>
+                            <tr>
+                                <th>{t("cvsList.studentName")}</th>
+                                <th>{t("cvsList.name")}</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cvs.map((cv) => {return <CvRow key={cv.id} cv={cv} userType={userType} updateCvsState={updateCvsState}/>})}
+                        </tbody>
+                    </Table>
+                    :
+                    <p>{t("cvsList.noCvs")}</p>
+            }
+        </>
+    )
+}
+
+export default CvsList;
