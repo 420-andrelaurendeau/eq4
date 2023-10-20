0f97373ccd7651456c8cb344de77c6ceaa9d185b
joshua kuzak <joshua_938@outlook.com>
2023-10-20 15:48:40 -0400

    EQ-4-111-Added empty check for application array

--- audace-frontend/src/components/ApplicationsList/index.tsx
+++ audace-frontend/src/components/ApplicationsList/index.tsx
@@ -9,21 +9,30 @@ interface Props {
 const ApplicationsList = ({ applications }: Props) => {
   return (
     <>
-      <h2>ApplicationsList</h2>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Position title</th>
-            <th>Organization</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {applications.map((application) => (
-            <ApplicationRow key={application.id} application={application} />
-          ))}
-        </tbody>
-      </Table>
+      {applications.length > 0 ? (
+        <>
+          <h2>ApplicationsList</h2>
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>Position title</th>
+                <th>Organization</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {applications.map((application) => (
+                <ApplicationRow
+                  key={application.id}
+                  application={application}
+                />
+              ))}
+            </tbody>
+          </Table>
+        </>
+      ) : (
+        <h2>No applications have been made</h2>
+      )}
     </>
   );
 };
