696c0b3cceb51775a596c84c17909a25c9b100ee
Jesus9388 <joshua_938@outlook.com>
2023-10-26 11:34:21 -0400

    ÃEQ-4-174-Unfucked the shit

--- audace-backend/src/main/java/com/equipe4/audace/model/session/StudentSession.java
+++ audace-backend/src/main/java/com/equipe4/audace/model/session/StudentSession.java
@@ -3,6 +3,8 @@ package com.equipe4.audace.model.session;
 import com.equipe4.audace.model.Student;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -13,6 +15,9 @@ public class StudentSession {
     @Id
     private Long id;
 
+    @OneToOne
     private Student student;
+
+    @ManyToOne
     private Session session;
 }
