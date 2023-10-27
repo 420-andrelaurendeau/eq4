24cf448939f46d02a0319f304b24f3e2d88bdbb5
joshua kuzak <joshua_938@outlook.com>
2023-10-27 13:20:07 -0400

    EQ-4-173-Made cool stuff

--- audace-frontend/src/components/SessionSelector/SelectorOption/index.tsx
+++ audace-frontend/src/components/SessionSelector/SelectorOption/index.tsx
@@ -1,14 +1,17 @@
 import { Dropdown } from "react-bootstrap";
 import { Session } from "../../../model/session";
 import { formatDate } from "../../../services/formatService";
+import { useSessionContext } from "../../../contextsholders/providers/SessionContextHolder";
 
 interface Props {
   session: Session;
 }
 
 const SelectorOption = ({ session }: Props) => {
+  const { chosenSession } = useSessionContext();
+
   return (
-    <Dropdown.Item eventKey={`${session.id}`}>
+    <Dropdown.Item eventKey={`${session.id}`} active={chosenSession?.id === session.id}>
       Session {formatDate(session.startDate)} - {formatDate(session.endDate)}
     </Dropdown.Item>
   );
