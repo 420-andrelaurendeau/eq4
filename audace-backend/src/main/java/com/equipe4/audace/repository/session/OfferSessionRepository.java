e76166b6945db541dd6c6ab2778bcbacfd4f73c7
joshua kuzak <joshua_938@outlook.com>
2023-10-29 13:13:33 -0400

    EQ-4-174-Fixed broken delete bc of OfferSession foreign key

--- audace-backend/src/main/java/com/equipe4/audace/repository/session/OfferSessionRepository.java
+++ audace-backend/src/main/java/com/equipe4/audace/repository/session/OfferSessionRepository.java
@@ -6,8 +6,10 @@ import com.equipe4.audace.model.session.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OfferSessionRepository extends JpaRepository<OfferSession, Long> {
     List<OfferSession> findAllByOfferInAndSessionId(List<Offer> offers, Long sessionId);
     boolean existsByOfferAndSession(Offer offer, Session session);
+    Optional<OfferSession> findByOffer(Offer offer);
 }
