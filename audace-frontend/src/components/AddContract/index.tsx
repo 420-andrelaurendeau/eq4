a15e3af123e54c87171abf79a221e902b6ada113
Artem <artemvandalovski@gmail.com>
2023-10-29 20:38:42 -0400

    addcontract

--- audace-frontend/src/components/AddContract/index.tsx
+++ audace-frontend/src/components/AddContract/index.tsx
@@ -3,7 +3,7 @@ import http from "../../constants/http";
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import FormInput from '../FormInput';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container } from 'react-bootstrap';
 import { managerCreateContract } from '../../services/contractService';
 import { Contract } from '../../model/contract';
 import { Employer } from '../../model/user';
@@ -13,7 +13,7 @@ const AddContract = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const [errors, setErrors] = useState<string[]>([]);
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [officeName, setOfficeName] = useState('');
   const [startHour, setStartHour] = useState('');
@@ -26,12 +26,12 @@ const AddContract = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // setIsLoading(true);
+    setIsLoading(true);
     try {
 
     } catch (error) {
       console.error(error);
-      // setIsLoading(false);
+      setIsLoading(false);
     }
   };
 
@@ -45,17 +45,62 @@ const AddContract = () => {
   };
 
   return (
-    <Form className='form m-5' onSubmit={handleSubmit}>
-      <h1>Add Contract</h1>
-      <FormInput
-        label="contract.officeName"
-        value={officeName}
-        onChange={(e) => setOfficeName(e.target.value)}
-        controlId="formBasicOfficeName"
-        errors={errors}
-        formError={"contract.errors.emptyOfficeName"}
-      />
-    </Form>
+    <Container>
+      <h1>{t('manager.createContract.title')}</h1>
+      <Form onSubmit={handleSubmit}>
+        <FormInput
+          label="manager.createContract.officeName"
+          value={officeName}
+          onChange={(e) => setOfficeName(e.target.value)}
+          controlId="formBasicOfficeName"
+          errors={errors}
+          formError={"contract.errors.emptyOfficeName"}
+        />
+        <FormInput
+          label="manager.createContract.startHour"
+          value={startHour}
+          onChange={(e) => setStartHour(e.target.value)}
+          controlId="formBasicStartHour"
+          errors={errors}
+          formError={"contract.errors.emptyStartHour"}
+        />
+        <FormInput
+          label="manager.createContract.endHour"
+          value={endHour}
+          onChange={(e) => setEndHour(e.target.value)}
+          controlId="formBasicEndHour"
+          errors={errors}
+          formError={"contract.errors.emptyEndHour"}
+        />
+        <FormInput
+          label="manager.createContract.totalHoursPerWeek"
+          value={totalHoursPerWeek}
+          onChange={(e) => setTotalHoursPerWeek(e.target.value)}
+          controlId="formBasicTotalHoursPerWeek"
+          errors={errors}
+          formError={"contract.errors.emptyTotalHoursPerWeek"}
+        />
+        <FormInput
+          label="manager.createContract.salary"
+          value={salary}
+          onChange={(e) => setSalary(e.target.value)}
+          controlId="formBasicSalary"
+          errors={errors}
+          formError={"contract.errors.emptySalary"}
+        />
+        <FormInput
+          label="manager.createContract.internTasksAndResponsibilities"
+          value={internTasksAndResponsibilities}
+          onChange={(e) => setInternTasksAndResponsibilities(e.target.value)}
+          controlId="formBasicInternTasksAndResponsibilities"
+          errors={errors}
+          formError={"contract.errors.emptyInternTasksAndResponsibilities"}
+        />
+        <Button variant="primary" type="submit" disabled={isLoading}>
+          {isLoading ? t('common.loading') : t('common.submit')}
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
