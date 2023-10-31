ffaf72ba55aaa69f6b079829df431d820b73aa7e
joshua kuzak <joshua_938@outlook.com>
2023-10-31 14:02:01 -0400

    Added test for FileUploader

--- audace-frontend/src/tests/components/FileUploader.test.tsx
+++ audace-frontend/src/tests/components/FileUploader.test.tsx
@@ -1,19 +1,42 @@
 import { render, screen } from "@testing-library/react";
 import FileUploader from "../../components/FileUploader";
 import { Student } from "../../model/user";
+import userEvent from "@testing-library/user-event";
 
-it("should render normally", () => {
-  const student: Student = {
-    studentNumber: "1234567890",
-    firstName: "John",
-    lastName: "Doe",
-    email: "yaintizit@email.com",
-    phone: "1234567890",
-    address: "1234 Main St",
-    password: "password",
-    type: "student",
-  };
+const student: Student = {
+  studentNumber: "1234567890",
+  firstName: "John",
+  lastName: "Doe",
+  email: "yaintizit@email.com",
+  phone: "1234567890",
+  address: "1234 Main St",
+  password: "password",
+  type: "student",
+};
+
+it("should have a submit button", () => {
   render(<FileUploader student={student} />);
 
   screen.getByText(/upload.submit/i);
+});
+
+it("should have a file input", () => {
+  render(<FileUploader student={student} />);
+
+  screen.getByLabelText(/upload.file/i);
+});
+
+it("should submit a file", async () => {
+  render(<FileUploader student={student} />);
+
+  const str = JSON.stringify({ name: "test" });
+  const blob = new Blob([str], { type: "application/json" });
+  const file = new File([blob], "test.json", { type: "application/json" });
+
+  const input = screen.getByLabelText(/upload.file/i);
+
+  userEvent.upload(input, file);
+  userEvent.click(screen.getByText(/upload.submit/i));
+
+  await screen.findByText(/upload.success/i);
 });
