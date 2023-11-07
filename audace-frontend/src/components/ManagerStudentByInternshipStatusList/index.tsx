fc068980c5541ad1b5b7a5740a0bebf0a9adb150
p3terson7 <psarateanu@hotmail.ca>
2023-11-07 11:36:41 -0500

    translation in table

--- audace-frontend/src/components/ManagerStudentByInternshipStatusList/index.tsx
+++ audace-frontend/src/components/ManagerStudentByInternshipStatusList/index.tsx
@@ -3,11 +3,13 @@ import { Student, StudentsByInternshipFoundStatus } from "../../model/user";
 import { getStudentsByInternshipStatus } from "../../services/managerService";
 import {Col, Form, Row, Table} from "react-bootstrap";
 import ManagerStudentByInternshipStatusRow from "./ManagerStudentByInternshipStatusRow";
+import {useTranslation} from "react-i18next";
 
 const ManagerStudentByInternshipStatusList = () => {
     const [studentsByInternshipStatus, setStudentsByInternshipStatus] = useState<
         StudentsByInternshipFoundStatus
     >();
+    const {t} = useTranslation();
     const [selectedOption, setSelectedOption] = useState("studentsWithPendingResponse");
     const [searchText, setSearchText] = useState("");
     const [filteredStudents, setFilteredStudents] = useState<Student[]>([]);
@@ -44,22 +46,22 @@ const ManagerStudentByInternshipStatusList = () => {
         <>
             <Row style={{ paddingBottom: "10px" }}>
             <Col>
-                <h3>Students By Internship Status</h3>
+                <h3>{t("studentsByInternship.title")}</h3>
             </Col>
             <Col className="d-flex">
                 <Col style={{ flex: "1" }}>
                     <select className="select-custom" value={selectedOption} onChange={handleDropdownChange}>
-                        <option value="studentsWithPendingResponse">Pending</option>
-                        <option value="studentsWithAcceptedResponse">Accepted</option>
-                        <option value="studentsWithRefusedResponse">Refused</option>
-                        <option value="studentsWithoutApplications">No applications</option>
+                        <option value="studentsWithPendingResponse">{t("studentsByInternship.row.statusValues.PENDING")}</option>
+                        <option value="studentsWithAcceptedResponse">{t("studentsByInternship.row.statusValues.ACCEPTED")}</option>
+                        <option value="studentsWithRefusedResponse">{t("studentsByInternship.row.statusValues.REFUSED")}</option>
+                        <option value="studentsWithoutApplications">{t("studentsByInternship.row.statusValues.NO_APPLICATIONS")}</option>
                     </select>
                 </Col>
                 <Col style={{ flex: "2" }}>
                     <Form.Control
                         className="custom-search-input"
                         type="text"
-                        placeholder="Search by name, number, or department"
+                        placeholder={t("studentsByInternship.searchBarPlaceholder")}
                         value={searchText}
                         onChange={(e) => {
                             setSearchText(e.target.value);
@@ -76,10 +78,10 @@ const ManagerStudentByInternshipStatusList = () => {
                     <Table className="table-custom" striped bordered hover size="sm">
                         <thead className="table-custom">
                         <tr>
-                            <th>Name</th>
-                            <th>Number</th>
-                            <th>Department</th>
-                            <th>Status</th>
+                            <th>{t("studentsByInternship.row.nameTh")}</th>
+                            <th>{t("studentsByInternship.row.studentNumberTh")}</th>
+                            <th>{t("studentsByInternship.row.departmentTh")}</th>
+                            <th>{t("studentsByInternship.row.statusTh")}</th>
                         </tr>
                         </thead>
                         <tbody className="table-custom">
@@ -90,7 +92,7 @@ const ManagerStudentByInternshipStatusList = () => {
                     </Table>
                 </div>
             ) : (
-                <p>No student</p>
+                <p>{t("studentsByInternship.noStudents")}</p>
             )}
         </>
     );
