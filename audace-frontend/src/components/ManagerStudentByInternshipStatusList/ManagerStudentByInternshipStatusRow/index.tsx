e1699c2e792a03197c94d735f8d44dc26a409b9f
p3terson7 <psarateanu@hotmail.ca>
2023-11-07 11:45:59 -0500

    it liiives

--- audace-frontend/src/components/ManagerStudentByInternshipStatusList/ManagerStudentByInternshipStatusRow/index.tsx
+++ audace-frontend/src/components/ManagerStudentByInternshipStatusList/ManagerStudentByInternshipStatusRow/index.tsx
@@ -1,25 +1,37 @@
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
-import {Student} from "../../../model/user";
+import { Student } from "../../../model/user";
 
 interface Props {
     student: Student;
     status: string;
 }
-const ManagerStudentByInternshipStatusRow = ({student, status}: Props) => {
-    const navigate = useNavigate();
+
+const ManagerStudentByInternshipStatusRow = ({ student, status }: Props) => {
     const { t } = useTranslation();
+
+    const getStatusTranslation = (status: string) => {
+        switch (status) {
+            case "PENDING":
+                return t("studentsByInternship.row.statusValues.PENDING");
+            case "ACCEPTED":
+                return t("studentsByInternship.row.statusValues.ACCEPTED");
+            case "REFUSED":
+                return t("studentsByInternship.row.statusValues.REFUSED");
+            case "INTERN":
+                return t("studentsByInternship.row.statusValues.INTERN");
+            case "NO_APPLICATIONS":
+                return t("studentsByInternship.row.statusValues.NO_APPLICATIONS");
+            default:
+                return status;
+        }
+    };
 
     return (
         <tr>
             <td>{student.lastName}, {student.firstName}</td>
             <td>{student.studentNumber}</td>
-            <td>
-                {student.department!.name}
-            </td>
-            <td>
-                {status}
-            </td>
+            <td>{student.department!.name}</td>
+            <td>{getStatusTranslation(status)}</td>
         </tr>
     );
 };
