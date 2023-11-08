bcd2a75a06968acc37b0f6ca6392275d3d7ffacf
joshua kuzak <joshua_938@outlook.com>
2023-11-08 12:50:32 -0500

    EQ-4-196-Added tests for OfferButtons

--- audace-frontend/src/tests/components/OffersList/OfferButtons/OfferButtons.test.tsx
+++ audace-frontend/src/tests/components/OffersList/OfferButtons/OfferButtons.test.tsx
@@ -1,5 +1,67 @@
 import { render, screen } from "@testing-library/react";
+import OfferButtons from "../../../../components/OffersList/OfferRow/OfferButtons";
+import { Offer, OfferStatus } from "../../../../model/offer";
+import { UserType } from "../../../../model/user";
+import "@testing-library/jest-dom/extend-expect";
 
-it("temp", () => {
-    
-})
+const offer: Offer = {
+  id: 1,
+  title: "title",
+  description: "description",
+  internshipStartDate: new Date(),
+  internshipEndDate: new Date(),
+  offerEndDate: new Date(),
+  availablePlaces: 1,
+  department: {
+    id: 1,
+    code: "code",
+    name: "name",
+  },
+  employer: {
+    id: 1,
+    firstName: "firstName",
+    lastName: "lastName",
+    email: "email",
+    phone: "phone",
+    address: "address",
+    password: "password",
+    type: "employer",
+    organisation: "organisation",
+    position: "position",
+    extension: "extension",
+  },
+  offerStatus: OfferStatus.PENDING,
+};
+
+it("should render employer buttons when employer user type", () => {
+  jest
+    .spyOn(require("../../../../services/authService"), "getUserType")
+    .mockImplementation(() => UserType.Employer);
+
+  render(<OfferButtons disabled={false} offer={offer} />);
+
+  const editButton = screen.getByText(/offersList.editButton/i);
+  expect(editButton).toBeInTheDocument();
+});
+
+it("should render student buttons when student user type", () => {
+  jest
+    .spyOn(require("../../../../services/authService"), "getUserType")
+    .mockImplementation(() => UserType.Student);
+
+  render(<OfferButtons disabled={false} offer={offer} />);
+
+  const applyButton = screen.getByText(/offersList.applyButton/i);
+  expect(applyButton).toBeInTheDocument();
+});
+
+it("should render manager buttons when manager user type", () => {
+  jest
+    .spyOn(require("../../../../services/authService"), "getUserType")
+    .mockImplementation(() => UserType.Manager);
+
+  render(<OfferButtons disabled={false} offer={offer} />);
+
+  const acceptButton = screen.getByText(/offersList.acceptButton/i);
+  expect(acceptButton).toBeInTheDocument();
+});
