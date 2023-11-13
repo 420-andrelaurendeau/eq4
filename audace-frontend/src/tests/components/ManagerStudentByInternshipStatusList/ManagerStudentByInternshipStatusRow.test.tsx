164bd6b9f2b65494b59d05a10eecf0ed640c7989
Jesus9388 <joshua_938@outlook.com>
2023-11-13 14:08:08 -0500

    Fixed warnings

--- audace-frontend/src/tests/components/ManagerStudentByInternshipStatusList/ManagerStudentByInternshipStatusRow.test.tsx
+++ audace-frontend/src/tests/components/ManagerStudentByInternshipStatusList/ManagerStudentByInternshipStatusRow.test.tsx
@@ -2,14 +2,24 @@ import { render, screen } from "@testing-library/react";
 import ManagerStudentByInternshipStatusRow from "../../../components/ManagerStudentByInternshipStatusList/ManagerStudentByInternshipStatusRow";
 import { student } from "../testUtils/testUtils";
 import "@testing-library/jest-dom/extend-expect";
+import { Table } from "react-bootstrap";
 
 it("should render student values", () => {
   render(
-    <ManagerStudentByInternshipStatusRow student={student} status="pending" />
+    <Table>
+      <tbody>
+        <ManagerStudentByInternshipStatusRow
+          student={student}
+          status="pending"
+        />
+      </tbody>
+    </Table>
   );
 
   expect(screen.getByText(/Doe, John/i)).toBeInTheDocument();
   expect(screen.getByText(/123456789/i)).toBeInTheDocument();
   expect(screen.getByText(/Computer Science/i)).toBeInTheDocument();
-  expect(screen.getByText(/studentsByInternship.row.statusValues.pending/i)).toBeInTheDocument();
+  expect(
+    screen.getByText(/studentsByInternship.row.statusValues.pending/i)
+  ).toBeInTheDocument();
 });
