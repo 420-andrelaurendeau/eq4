b39d400e55be02a1cfaf13d6a7609c2e29c6dfb1
shouvik <shouvikdeebee@gmail.com>
2023-11-15 19:14:14 -0500

    buttons only appear for the right person and disappear when signed

--- audace-frontend/src/components/SignContract/index.tsx
+++ audace-frontend/src/components/SignContract/index.tsx
@@ -9,170 +9,187 @@ import { getUserById } from '../../services/userService';
 import { ManagerSignContract } from '../../services/contractService';
 
 const SignContract = () => {
-  const { id } = useParams();
-  const [contract, setContract] = useState<Contract | null>(null);
-  const [UserType, setUserType] = useState<string | null>(null);
+    const { id } = useParams();
+    const [contract, setContract] = useState<Contract | null>(null);
+    const [UserType, setUserType] = useState<string | null>(null);
 
-  useEffect(() => {
+    useEffect(() => {
 
-    (async () => {
-        try {
-            const userId = parseInt(getUserId() || '0');
-            if (!userId) {
-                console.error("Invalid user ID");
-                return;
+        (async () => {
+            try {
+                const userId = parseInt(getUserId() || '0');
+                if (!userId) {
+                    console.error("Invalid user ID");
+                    return;
+                }
+
+                const response = await getUserById(userId);
+                setUserType(response.data.type || null);
+            } catch (error) {
+                console.error("Error fetching user:", error);
             }
+        })();
 
-            const response = await getUserById(userId);
-            setUserType(response.data.type || null);
-        } catch (error) {
-            console.error("Error fetching user:", error);
+        if (id) {
+            getContractById(parseInt(id))
+                .then((response) => {
+                    console.log("Fetched contract:", response.data);
+                    setContract(response.data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching contract:", error);
+                });
         }
-    })();
 
-    if (id) {
-        getContractById(parseInt(id))
-            .then((response) => {
-                console.log("Fetched contract:", response.data); 
-                setContract(response.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching contract:", error);
-            });
+    }, [id]);
+
+    if (!contract) {
+        return <p>Loading contract...</p>;
     }
 
-  }, [id]);
-
-  if (!contract) {
-    return <p>Loading contract...</p>;
-  }
 
 
+    const { supervisor, application, startHour, endHour, totalHoursPerWeek, salary } = contract;
+    const { offer } = application || {};
+    const { employer } = offer || {};
+    const { student } = application.cv || {};
 
-const { supervisor, application, startHour, endHour, totalHoursPerWeek, salary } = contract;
-const { offer } = application || {};
-const { employer } = offer || {};
-const { student } = application.cv || {};
-
-function handleSign(arg0: string) {
-    if (arg0 === 'manager') {
-        ManagerSignContract(parseInt(getUserId() || '0') || 0, contract?.id!);
-        console.log(contract);
-    } else if (arg0 === 'employer') {
-        console.log('employer');
-    } else if (arg0 === 'student') {
-        console.log('student');
+    function handleSign(arg0: string) {
+        if (arg0 === 'manager') {
+            ManagerSignContract(parseInt(getUserId() || '0') || 0, contract?.id!);
+            console.log(contract);
+        } else if (arg0 === 'employer') {
+            console.log('employer');
+        } else if (arg0 === 'student') {
+            console.log('student');
+        }
     }
-}
 
-  return (
-    <Container className="mt-4">
-      <Row className="justify-content-center">
-        <Col md={8}>
-          <Card className="mb-3">
-            <Card.Header as="h5">Contract Details</Card.Header>
-            <Card.Body>
-              <ListGroup variant="flush">
-                <ListGroupItem><strong>Start Hour:</strong> {startHour}</ListGroupItem>
-                <ListGroupItem><strong>End Hour:</strong> {endHour}</ListGroupItem>
-                <ListGroupItem><strong>Total Hours Per Week:</strong> {totalHoursPerWeek}</ListGroupItem>
-                <ListGroupItem><strong>Salary:</strong> {salary}</ListGroupItem>
-              </ListGroup>
-            </Card.Body>
-          </Card>
+    return (
+        <Container className="mt-4">
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    <Card className="mb-3">
+                        <Card.Header as="h5">Contract Details</Card.Header>
+                        <Card.Body>
+                            <ListGroup variant="flush">
+                                <ListGroupItem><strong>Start Hour:</strong> {startHour}</ListGroupItem>
+                                <ListGroupItem><strong>End Hour:</strong> {endHour}</ListGroupItem>
+                                <ListGroupItem><strong>Total Hours Per Week:</strong> {totalHoursPerWeek}</ListGroupItem>
+                                <ListGroupItem><strong>Salary:</strong> {salary}</ListGroupItem>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
 
-          <Card className="mb-3">
-            <Card.Header as="h5">Supervisor Details</Card.Header>
-            <Card.Body>
-              <ListGroup variant="flush">
-                <ListGroupItem><strong>Name:</strong> {`${supervisor.firstName} ${supervisor.lastName}`}</ListGroupItem>
-                <ListGroupItem><strong>Position:</strong> {supervisor.position}</ListGroupItem>
-                <ListGroupItem><strong>Email:</strong> {supervisor.email}</ListGroupItem>
-                <ListGroupItem><strong>Phone:</strong> {supervisor.phone}</ListGroupItem>
-              </ListGroup>
-            </Card.Body>
-          </Card>
+                    <Card className="mb-3">
+                        <Card.Header as="h5">Supervisor Details</Card.Header>
+                        <Card.Body>
+                            <ListGroup variant="flush">
+                                <ListGroupItem><strong>Name:</strong> {`${supervisor.firstName} ${supervisor.lastName}`}</ListGroupItem>
+                                <ListGroupItem><strong>Position:</strong> {supervisor.position}</ListGroupItem>
+                                <ListGroupItem><strong>Email:</strong> {supervisor.email}</ListGroupItem>
+                                <ListGroupItem><strong>Phone:</strong> {supervisor.phone}</ListGroupItem>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
 
-          {student && (
-            <Card className="mb-3">
-              <Card.Header as="h5">Student Details</Card.Header>
-              <Card.Body>
-                <ListGroup variant="flush">
-                  <ListGroupItem><strong>Name:</strong> {`${student.firstName} ${student.lastName}`}</ListGroupItem>
-                  <ListGroupItem><strong>ID:</strong> {student.studentNumber}</ListGroupItem>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          )}
+                    {student && (
+                        <Card className="mb-3">
+                            <Card.Header as="h5">Student Details</Card.Header>
+                            <Card.Body>
+                                <ListGroup variant="flush">
+                                    <ListGroupItem><strong>Name:</strong> {`${student.firstName} ${student.lastName}`}</ListGroupItem>
+                                    <ListGroupItem><strong>ID:</strong> {student.studentNumber}</ListGroupItem>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    )}
 
-          {employer && (
-            <Card className="mb-3">
-              <Card.Header as="h5">Employer Details</Card.Header>
-              <Card.Body>
-                <ListGroup variant="flush">
-                  <ListGroupItem><strong>Name:</strong> {`${employer.firstName} ${employer.lastName}`}</ListGroupItem>
-                  <ListGroupItem><strong>Company:</strong> {employer.organisation}</ListGroupItem>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          )}
+                    {employer && (
+                        <Card className="mb-3">
+                            <Card.Header as="h5">Employer Details</Card.Header>
+                            <Card.Body>
+                                <ListGroup variant="flush">
+                                    <ListGroupItem><strong>Name:</strong> {`${employer.firstName} ${employer.lastName}`}</ListGroupItem>
+                                    <ListGroupItem><strong>Company:</strong> {employer.organisation}</ListGroupItem>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    )}
 
-          {offer && (
-            <Card>
-              <Card.Header as="h5">Offer Details</Card.Header>
-              <Card.Body>
-                <ListGroup variant="flush">
-                  <ListGroupItem><strong>Title:</strong> {offer.title}</ListGroupItem>
-                    <ListGroupItem><strong>Description:</strong> {offer.description}</ListGroupItem>
-                  <ListGroupItem><strong>Internship Start Date:</strong> {offer.internshipStartDate.toString()}</ListGroupItem>
-                  <ListGroupItem><strong>Internship End Date:</strong> {offer.internshipEndDate.toString()}</ListGroupItem>
-                </ListGroup>
-                
-              </Card.Body>
-              
-            </Card>
-          )}
+                    {offer && (
+                        <Card>
+                            <Card.Header as="h5">Offer Details</Card.Header>
+                            <Card.Body>
+                                <ListGroup variant="flush">
+                                    <ListGroupItem><strong>Title:</strong> {offer.title}</ListGroupItem>
+                                    <ListGroupItem><strong>Description:</strong> {offer.description}</ListGroupItem>
+                                    <ListGroupItem><strong>Internship Start Date:</strong> {offer.internshipStartDate.toString()}</ListGroupItem>
+                                    <ListGroupItem><strong>Internship End Date:</strong> {offer.internshipEndDate.toString()}</ListGroupItem>
+                                </ListGroup>
 
-<Card className="mt-3">
-        <Card.Header as="h5">Signature</Card.Header>
-        <Card.Body>
-          <ListGroup>
-            <ListGroupItem className="d-flex justify-content-between align-items-center">
-              Manager's Signature
-              <div>
-                <Button variant="secondary" onClick={() => handleSign('manager')} disabled={UserType !== 'manager'}>
-                  Sign
-                </Button>
-                <span className="ms-2">Signed on: {/* Display manager's sign date */}</span>
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className="d-flex justify-content-between align-items-center">
-              Employer's Signature
-              <div>
-                <Button variant="secondary" onClick={() => handleSign('employer')} disabled={UserType !== 'employer'}>
-                  Sign
-                </Button>
-                <span className="ms-2">Signed on: {/* Display employer's sign date */}</span>
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className="d-flex justify-content-between align-items-center">
-              Student's Signature
-              <div>
-                <Button variant="secondary" onClick={() => handleSign('student')} disabled={UserType !== 'student'}>
-                  Sign
-                </Button>
-                <span className="ms-2">Signed on: {/* Display student's sign date */}</span>
-              </div>
-            </ListGroupItem>
-          </ListGroup>
-        </Card.Body>
-      </Card>
+                            </Card.Body>
 
+                        </Card>
+                    )}
 
-        </Col>
-      </Row>
-    </Container>
-  );
+                    <Card className="mt-3">
+                        <Card.Header as="h5">Signature</Card.Header>
+                        <Card.Body>
+                            <ListGroup>
+                                <ListGroupItem className="d-flex justify-content-between align-items-center">
+                                    Manager's Signature
+                                    <div>
+                                        {UserType === 'manager' && !contract.managerSignature && (
+                                            <Button variant="secondary" onClick={() => handleSign('manager')}>
+                                                Sign
+                                            </Button>
+                                        )}
+                                        <span className="ms-2">
+                                            {contract.managerSignature
+                                                ? `Signed on: ${contract.managerSignature.signatureDate}`
+                                                : 'Not signed yet'}
+                                        </span>
+                                    </div>
+                                </ListGroupItem>
+                                <ListGroupItem className="d-flex justify-content-between align-items-center">
+                                    Employer's Signature
+                                    <div>
+                                        {UserType === 'employer' && !contract.employerSignature && (
+                                            <Button variant="secondary" onClick={() => handleSign('employer')}>
+                                                Sign
+                                            </Button>
+                                        )}
+                                        <span className="ms-2">
+                                            {contract.employerSignature
+                                                ? `Signed on: ${contract.employerSignature.signatureDate}`
+                                                : 'Not signed yet'}
+                                        </span>
+                                    </div>
+                                </ListGroupItem>
+                                <ListGroupItem className="d-flex justify-content-between align-items-center">
+                                    Student's Signature
+                                    <div>
+                                        {UserType === 'student' && !contract.studentSignature && (
+                                            <Button variant="secondary" onClick={() => handleSign('student')}>
+                                                Sign
+                                            </Button>
+                                        )}
+                                        <span className="ms-2">
+                                            {contract.studentSignature
+                                                ? `Signed on: ${contract.studentSignature.signatureDate}`
+                                                : 'Not signed yet'}
+                                        </span>
+                                    </div>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default SignContract;
